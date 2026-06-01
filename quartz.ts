b10b0135bf3261526a7e7889c391a4e973ff2c8e
Yyax13 <Yyax13@proton.me>
2026-06-01 17:24:10 -0300

    Quartz sync: Jun 1, 2026, 5:24 PM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,16 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
 const config = await loadQuartzConfig()
+config.configuration.theme.typography = {
+      header: {
+        name: "Cormorant Garamond",
+        includeItalic: false,
+      },
+      body: {
+        name: "Noto Serif",
+        includeItalic: false,
+      },
+      code: "IBM Plex Mono",
+    };
 export default config
 export const layout = await loadQuartzLayout()
