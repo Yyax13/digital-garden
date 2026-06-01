27deeb6e3f7ad19f64c60d5d5a6af43bbecd1804
Yyax13 <Yyax13@proton.me>
2026-06-01 14:52:13 -0300

    Quartz sync: Jun 1, 2026, 2:52 PM

---
+++ quartz.ts
@@ -0,0 +1,20 @@
+import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+let config = await loadQuartzConfig()
+config.configuration.theme.typography = {
+      header: {
+        name: "Felipa",
+        weights: [400],
+        includeItalic: false,
+
+      },
+      body: {
+        name: "Metamorphous",
+        weights: [400],
+        includeItalic: false,
+
+      },
+      code: "IBM Plex Mono",
+    };
+export default config
+export const layout = await loadQuartzLayout()
